9f841bdaa2a5f8149668836151f3eada5be1bbe8
Cedric Njanga <ced.njg@gmail.com>
2016-11-21 17:11:52 +0100

    db issu solve

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = %w(photography movie fx light)
 
+Product.delete_all
 10.times do
-  Product.create!(name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph, price: Faker::Commerce.price, category: categories.sample )
+  Product.create!(name: Faker::Commerce.product_name, description: Faker::Lorem.paragraph, price: Faker::Commerce.price, category: Product::CATEGORY )
 end
