e8b8313e6110ce1b24fe4a438137b9656d8bbd69
Cedric Njanga <ced.njg@gmail.com>
2016-11-22 17:25:39 +0100

    dont know why my schema file was modified

--- db/schema.rb
+++ db/schema.rb
@@ -38,10 +38,8 @@ ActiveRecord::Schema.define(version: 20161122151130) do
     t.boolean  "available"
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
-    t.boolean  "hidden",      default: false
-<<<<<<< HEAD
-=======
     t.integer  "user_id"
+    t.boolean  "hidden",      default: false
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
@@ -55,7 +53,6 @@ ActiveRecord::Schema.define(version: 20161122151130) do
     t.datetime "updated_at",                     null: false
     t.index ["product_id"], name: "index_rents_on_product_id", using: :btree
     t.index ["user_id"], name: "index_rents_on_user_id", using: :btree
->>>>>>> master
   end
 
   create_table "users", force: :cascade do |t|
