8cdc674342aa62e8899829e07f2978122403aa3c
Bousseau Romain <romainbousseau1@gmail.com>
2016-11-22 17:48:31 +0100

    resolved shit

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   mount Attachinary::Engine => "/attachinary"
+  resources :users, only: [ :edit, :update, :show ]
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   resources :products do
-
     resources :rents, only: [:show, :new, :create]
-
     get 'undisplay', to: "products#undisplay"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
