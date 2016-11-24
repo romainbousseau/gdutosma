73eb0d7e665914866472a6627f8d7d9cbd8ed2cc
stephane Robert <rob.steph@free.fr>
2016-11-24 16:00:55 +0100

    add helper for missing photo

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,12 @@
 module ProductsHelper
+
+  def product_card_background(product)
+    if product.photos?
+      cl_image_path(product.photos.first.path)
+    elsif product.category == "Sound"
+      asset_url("sound.png")
+    elsif product.category == "Lights"
+      asset_url("lights.png")
+    end
+  end
 end
