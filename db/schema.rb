eb9898203a94380ba576fc6b8088d4f099d659a4
Bousseau Romain <romainbousseau1@gmail.com>
2016-11-24 12:06:53 +0100

    added geo fields to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124095534) do
+ActiveRecord::Schema.define(version: 20161124110336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(version: 20161124095534) do
     t.datetime "token_expiry"
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "zip_code"
+    t.string   "city"
+    t.string   "country"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
