ffcac22446c7111530b21a9a4d63516d666a8b66
Bousseau Romain <romainbousseau1@gmail.com>
2016-11-25 14:22:10 +0100

    fixed recap email

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,6 +1,6 @@
 class RentsController < ApplicationController
   before_action :find_product, only: [:show, :new, :create ]
-  after_validate :send_recap_email
+  after_filter :send_recap_email, only: [ :validate ]
   def show
     @owner = User.find(@product.user)
     @rent = Rent.find(params[:id])
