2222e2fc80e8be0f916eb0025b282febc2817cfb
Cedric Njanga <ced.njg@gmail.com>
2016-11-25 14:27:15 +0100

    rent issue solved

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,18 @@
 module UsersHelper
+
+  def profile_complete?
+    parameters_to_avoid = ["reset_password_token", "reset_password_sent_at", "remember_created_at", "provider", "uid", "facebook_picture_url", "token", "token_expiry"]
+    boolean = true
+    current_user.attributes.each do |key, value|
+      unless parameters_to_avoid.include?(key)
+        if value.nil?
+        boolean = false
+        break
+        else
+        boolean = true
+        end
+      end
+    end
+    boolean
+  end  
 end
